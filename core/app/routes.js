8c32a86d93440db60e6f399dbc67e509b28b5dee
PABLO A. LOPEZ DOMOWICZ <palopezd@us.ibm.com>
2016-12-27 17:31:35 -0500

    added facebook login to core

--- core/app/routes.js
+++ core/app/routes.js
@@ -34,7 +34,15 @@ module.exports = function(app, passport) {
             user : req.user // get the user out of session and pass to template
         });
     });
-    
+    app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+    // handle the callback after facebook has authenticated the user
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect : '/profile',
+            failureRedirect : '/'
+        }));
+
     app.get('/logout', function(req, res) {
         req.logout();
         res.redirect('/');
